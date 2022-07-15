0d5556307bfd6ae6d380bb9b16318d9c3cdc91ad
kibromkingo <108776587+kibromkingo@users.noreply.github.com>
2022-07-14 23:33:56 -0400

    i did it

---
+++ MK23/src/Mk1423/MK0723.java
@@ -0,0 +1,10 @@
+package Mk1423;
+
+public class MK0723 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("time to time");
+	}
+
+}
---
+++ mk07/src/mk07/km07.java
@@ -0,0 +1,10 @@
+package mk07;
+
+public class km07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gog is good");
+	}
+
+}
